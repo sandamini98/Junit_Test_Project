746cd33944b9f19b9ba39c2dc944da06994cae7b
sandamini98 <kgcsandamini@gmail.com>
2024-03-28 22:17:11 +0530

    annotations

---
+++ src/main/java/annotations/Child.java
@@ -0,0 +1,10 @@
+package annotations;
+
+public class Child extends Parent {
+
+    @Override
+    public void someMethod() {
+        System.out.println("Over riding method from the parent class");
+
+    }
+}
---
+++ src/main/java/annotations/Parent.java
@@ -0,0 +1,11 @@
+package annotations;
+
+public class Parent {
+    public void someMethod() {
+
+    }
+    @Deprecated
+    public void otherMethod() {
+        System.out.println("Doing all stuff....");
+    }
+}
